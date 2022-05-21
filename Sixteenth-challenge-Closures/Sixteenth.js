a73703334dc5da2bc25b7f8960ef86933d8237a1
Mopano <99732517+funnypar@users.noreply.github.com>
2022-05-21 19:02:08 +0430

    Mopano answer

--- Sixteenth-challenge-Closures/Sixteenth.js
+++ Sixteenth-challenge-Closures/Sixteenth.js
@@ -3,4 +3,8 @@
 (function () {
   const header = document.querySelector("h1");
   header.style.color = "red";
+
+  document.body.addEventListener("click", () => {
+    header.style.color = "blue";
+  });
 })();
