6bb2a5e6b0ec759b2153cd401d1fcbf1df4c3098
Mopano <99732517+funnypar@users.noreply.github.com>
2022-05-26 16:10:57 +0430

    Added task 3

--- Seventeenth-challenge-juliaAndKateDogs/Seventeenth.js
+++ Seventeenth-challenge-juliaAndKateDogs/Seventeenth.js
@@ -7,5 +7,10 @@ const checkDogs = function (arrayJ, arrayK) {
   const newJuliaDogs = arrayJ.slice(1, 3);
   const bothDogs = newJuliaDogs.concat(arrayK);
   console.log(bothDogs);
+  bothDogs.forEach(function (dog, i) {
+    dog >= 3
+      ? console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old.`)
+      : console.log(`Dog number ${i + 1} is still a puppy`);
+  });
 };
 checkDogs(juliaDogs, kateDogs);
