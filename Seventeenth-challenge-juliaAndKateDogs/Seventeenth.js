a056a649dbbcfde5692672ca767772ab11c912ed
Mopano <99732517+funnypar@users.noreply.github.com>
2022-05-26 15:56:25 +0430

    Add files via upload

---
+++ Seventeenth-challenge-juliaAndKateDogs/Seventeenth.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const juliaDogs = [3, 5, 2, 12, 7];
+const kateDogs = [4, 1, 15, 8, 3];
+
+const checkDogs = function (arrayJ, arrayK) {
+  const newJuliaDogs = arrayJ.slice(1, 3);
+  console.log(newJuliaDogs);
+};
+checkDogs(juliaDogs, kateDogs);
