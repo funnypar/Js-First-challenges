b6b96da72e331f96e96113da88435a9b623aeb73
Mopano <99732517+funnypar@users.noreply.github.com>
2022-05-26 16:03:48 +0430

    Added task 2

--- Seventeenth-challenge-juliaAndKateDogs/Seventeenth.js
+++ Seventeenth-challenge-juliaAndKateDogs/Seventeenth.js
@@ -5,6 +5,7 @@ const kateDogs = [4, 1, 15, 8, 3];
 
 const checkDogs = function (arrayJ, arrayK) {
   const newJuliaDogs = arrayJ.slice(1, 3);
-  console.log(newJuliaDogs);
+  const bothDogs = newJuliaDogs.concat(arrayK);
+  console.log(bothDogs);
 };
 checkDogs(juliaDogs, kateDogs);
