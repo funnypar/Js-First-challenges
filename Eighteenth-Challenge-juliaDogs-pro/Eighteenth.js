c08df1a374767dfccd26d132b14f4c7fa8c82fc6
Mopano <99732517+funnypar@users.noreply.github.com>
2022-05-28 16:33:15 +0430

    Added task 2

--- Eighteenth-Challenge-juliaDogs-pro/Eighteenth.js
+++ Eighteenth-Challenge-juliaDogs-pro/Eighteenth.js
@@ -4,8 +4,12 @@
 const test1 = [5, 2, 4, 1, 15, 8, 3];
 
 const calcAverageHumanAge = function (dogAges) {
+  //Task 1
   const humanAges = dogAges.map((age) => (age <= 2 ? age * 2 : 16 + age * 4));
-  return humanAges;
+  // return humanAges;
+  //Task 2
+  const filterDogs = humanAges.filter((human) => human < 18);
+  return filterDogs;
 };
 
 console.log(calcAverageHumanAge(test1));
