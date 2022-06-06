b6516148150433b996c0d5c98866ccf95c3fffdb
Mopano <99732517+funnypar@users.noreply.github.com>
2022-06-06 19:06:44 +0430

    Add files via upload

--- Twentieth-challenge-julia&kate/Twentieth.js
+++ Twentieth-challenge-julia&kate/Twentieth.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const dogs = [
+  { dog: 1, weight: 22, currentFood: 250, owners: ["Alice", "Bob"] },
+  { dog: 2, weight: 8, curFood: 200, owners: ["Matilda"] },
+  { dog: 3, weight: 13, curFood: 275, owners: ["Sarah", "John"] },
+  { dog: 4, weight: 32, curFood: 340, owners: ["Micheal"] },
+];
+
+//task1
+dogs.forEach((e) => {
+  e.recommended = Math.abs(Object.values(e)[1] ** 0.78 * 28);
+});
