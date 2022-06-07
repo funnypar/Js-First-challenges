744518c1ff01ddb88bd6e56d514e8f0ea1684c4e
Mopano <99732517+funnypar@users.noreply.github.com>
2022-06-07 13:47:01 +0430

    Added task 3

--- Twentieth-challenge-julia&kate/Twentieth.js
+++ Twentieth-challenge-julia&kate/Twentieth.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const dogs = [
-  { dog: 1, weight: 22, currentFood: 250, owners: ["Alice", "Bob"] },
+  { dog: 1, weight: 22, curFood: 250, owners: ["Alice", "Bob"] },
   { dog: 2, weight: 8, curFood: 200, owners: ["Matilda"] },
   { dog: 3, weight: 13, curFood: 275, owners: ["Sarah", "John"] },
   { dog: 4, weight: 32, curFood: 340, owners: ["Micheal"] },
@@ -19,3 +19,15 @@ dogs.find((cur) => {
     console.log(`${sarah}'s dog eats too much`);
   }
 });
+
+//task3
+
+const ownersEatTooMuch = [];
+const ownersEatTooLittle = [];
+dogs.forEach((e) => {
+  e.curFood > 250
+    ? ownersEatTooMuch.push(e.owners)
+    : ownersEatTooLittle.push(e.owners);
+});
+console.log(ownersEatTooMuch.flat());
+console.log(ownersEatTooLittle.flat());
