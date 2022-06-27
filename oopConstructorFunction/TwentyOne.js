1b682046ca0a7b019f741f7f1141cbbcc69465e7
Mopano <99732517+funnypar@users.noreply.github.com>
2022-06-27 12:57:21 +0430

    Added Final Task

--- oopConstructorFunction/TwentyOne.js
+++ oopConstructorFunction/TwentyOne.js
@@ -6,17 +6,21 @@ const Car = function (speed) {
 };
 
 const bmw = new Car(120);
-// console.log(bmw);
+const mercedes = new Car(95);
 
 //Task 2
 Car.prototype.accelerate = function () {
   console.log(`New speed after accelerate : ${this.speed + 10} km/h`);
 };
-bmw.accelerate();
-
 //Task 3
 Car.prototype.brake = function () {
   console.log(`New speed after brake : ${this.speed - 5} km/h
   `);
 };
+
+//BMW
+bmw.accelerate();
 bmw.brake();
+//Mercedes
+mercedes.accelerate();
+mercedes.brake();
