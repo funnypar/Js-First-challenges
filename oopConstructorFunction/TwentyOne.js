a8a407f68b596ff15c1ab482f9522bbcc90a4ec2
Mopano <99732517+funnypar@users.noreply.github.com>
2022-06-27 12:42:25 +0430

    Added Task 2

--- oopConstructorFunction/TwentyOne.js
+++ oopConstructorFunction/TwentyOne.js
@@ -1,7 +1,15 @@
 "use strict";
 
+//Task 1
 const Car = function (speed) {
   this.speed = speed;
 };
 
 const bmw = new Car(120);
+// console.log(bmw);
+
+//Task 2
+Car.prototype.accelerate = function () {
+  console.log(`New speed : ${this.speed + 10}km/h`);
+};
+bmw.accelerate();
