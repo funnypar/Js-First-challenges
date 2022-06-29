d57b6632e2a3b07f3ad04126c9d91de1cf4b8b76
Mopano <99732517+funnypar@users.noreply.github.com>
2022-06-29 13:51:40 +0430

    Added Task 3

--- TwentythreeOopChallengeClass/TwentyThree.js
+++ TwentythreeOopChallengeClass/TwentyThree.js
@@ -9,11 +9,22 @@ const EV = function (speed, charge) {
   Car.call(this, speed);
   this.charge = charge;
 };
+//needed object
+EV.prototype = Object.create(Car.prototype);
 // Added task 2
 EV.prototype.chargeBattery = function (chargeTo) {
   this.charge = chargeTo;
   console.log(`new charge is ${this.charge}`);
 };
+//Added Task 3
+EV.prototype.accelarate = function () {
+  this.speed += 20;
+  this.charge -= 1;
+  console.log(
+    `new speed is ${this.speed} km/h . And the charge is ${this.charge}%`
+  );
+};
 //Tesla
 const tesla = new EV(120, 20);
 tesla.chargeBattery(23);
+tesla.accelarate();
