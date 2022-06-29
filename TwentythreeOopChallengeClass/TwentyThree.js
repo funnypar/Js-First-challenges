c230e0b18bb83426ec2fedbdc94cdde64a0a48d6
Mopano <99732517+funnypar@users.noreply.github.com>
2022-06-29 13:36:04 +0430

    Added Task 1

--- TwentythreeOopChallengeClass/TwentyThree.js
+++ TwentythreeOopChallengeClass/TwentyThree.js
@@ -1,3 +1,11 @@
 "use strict";
 
 // Added task 1
+const Car = function (speed) {
+  this.speed = speed;
+};
+
+const EV = function (speed, charge) {
+  Car.call(this, speed);
+  this.charge = charge;
+};
