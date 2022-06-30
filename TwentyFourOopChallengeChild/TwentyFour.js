34033e6c7cb5a2508c9c9bd7cf6dd170364d2cbb
Mopano <99732517+funnypar@users.noreply.github.com>
2022-06-30 16:16:51 +0430

    Added Task 1

--- TwentyFourOopChallengeChild/TwentyFour.js
+++ TwentyFourOopChallengeChild/TwentyFour.js
@@ -26,21 +26,22 @@ class CarCl {
   }
 }
 
-const EV = function (make, speed, charge) {
-  Car.call(this, make, speed);
-  this.charge = charge;
-};
+//Added Task 1
+class EVCl extends CarCl {
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.charge = charge;
+  }
 
-EV.prototype = Object.create(Car.prototype);
+  set chargeBattery(chargeTo) {
+    this.charge = chargeTo;
+  }
 
-EV.prototype.chargeBattery = function (chargeTo) {
-  this.charge = chargeTo;
-};
-
-EV.prototype.accelarate = function () {
-  this.speed += 20;
-  this.charge--;
-  console.log(
-    `${this.make} is going at ${this.speed} km/h, with a charge of ${this.charge}`
-  );
-};
+  accelarate() {
+    this.speed += 20;
+    this.charge--;
+    console.log(
+      `${this.make} is going at ${this.speed} km/h, with a charge of ${this.charge}`
+    );
+  }
+}
