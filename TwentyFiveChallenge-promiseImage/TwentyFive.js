14ae5962e412b072376312ce8eea7f8875e27c22
Mopano <99732517+funnypar@users.noreply.github.com>
2022-07-16 00:58:11 +0430

    Added Task 1

--- TwentyFiveChallenge-promiseImage/TwentyFive.js
+++ TwentyFiveChallenge-promiseImage/TwentyFive.js
@@ -1,1 +1,18 @@
 "use strict";
+
+const createImg = function (path) {
+  return new Promise(function (resolve, reject) {
+    const image = document.createElement("img");
+    image.src = path;
+    image.addEventListener("load", function () {
+      document.body.append(image);
+      resolve(image);
+    });
+
+    image.addEventListener("error", function () {
+      reject(new Error("Image not be found"));
+    });
+  });
+};
+
+createImg("./46497ss.png");
